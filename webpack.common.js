be54f2c19b900660e22d2e161efc239f17f7a1a7
mac <smileua18@gmail.com>
2023-01-06 11:31:56 +0200

    added copy plugin

--- webpack.common.js
+++ webpack.common.js
@@ -1,23 +1,23 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const fs = require('fs');
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const ESLintPlugin = require('eslint-webpack-plugin');
 const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
-
+const CopyPlugin = require('copy-webpack-plugin');
 
 function getAllHbsRootFiles() {
-  const files = fs.readdirSync('./src/views/').filter(file => file.match(/.hbs$/));
+  const files = fs.readdirSync('./src/views/').filter((file) => file.match(/.hbs$/));
 
-  return files.map(file => {
+  return files.map((file) => {
     return new HtmlWebpackPlugin({
       template: `./src/views/${file}`,
-      filename: `./${ file.replace('.hbs', '.html') }`,
+      filename: `./${file.replace('.hbs', '.html')}`,
       // templateParameters: require('./src/data/index.json'),
       minify: false,
       inject: false,
-    })
-  })
+    });
+  });
 }
 
 module.exports = {
@@ -39,18 +39,18 @@ module.exports = {
           MiniCssExtractPlugin.loader,
           // Translates CSS into CommonJS
           {
-            loader: "css-loader",
+            loader: 'css-loader',
             options: {
               url: false,
             },
           },
           // Compiles Sass to CSS
-          { loader: "sass-loader", },
+          { loader: 'sass-loader' },
           {
             loader: 'sass-resources-loader',
             options: {
-              resources: './src/scss/core/wanted.scss'
-            }
+              resources: './src/scss/core/wanted.scss',
+            },
           },
         ],
       },
@@ -62,19 +62,17 @@ module.exports = {
           loader: 'babel-loader',
           options: {
             presets: ['@babel/preset-env', '@babel/preset-typescript'],
-            plugins: ['@babel/plugin-proposal-object-rest-spread']
-          }
-        }
+            plugins: ['@babel/plugin-proposal-object-rest-spread'],
+          },
+        },
       },
       /** handlebars loader **/
       {
         test: /\.hbs$/,
-        loader: "handlebars-loader",
+        loader: 'handlebars-loader',
         options: {
-          partialDirs: [
-            path.join(__dirname, 'src/views/partials')
-          ],
-        }
+          partialDirs: [path.join(__dirname, 'src/views/partials')],
+        },
       },
     ],
   },
@@ -84,11 +82,14 @@ module.exports = {
   plugins: [
     new ForkTsCheckerWebpackPlugin(),
     new ESLintPlugin({
-      extensions: ['ts']
+      extensions: ['ts'],
     }),
     /** выносим все стили с js в css файл **/
     new MiniCssExtractPlugin(),
     /** собераем html files **/
     ...getAllHbsRootFiles(),
+    new CopyPlugin({
+      patterns: [{ from: 'src/images', to: 'images' }],
+    }),
   ],
 };
